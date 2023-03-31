f7d43ea408ec860464a3bf18f4879447a9fcfa70
Rob Hanssen <rob.hanssen@outlook.com>
2023-03-31 17:32:08 -0400

    further digging

--- xmls_unnest.r
+++ xmls_unnest.r
@@ -10,6 +10,13 @@ null_weeder <- function(x) {
     }
 }
 
+return_height <- function(ht_text) {
+    lst <- str_remove_all(ht_text, '(\\")') %>% str_split(., pattern = "'")
+    sapply(seq_along(lst), function(i) {
+        as.numeric(lst[[i]][1]) * 0.3054 + 0.0254 * as.numeric(lst[[i]][2])
+    })
+}
+
 rawdata <-
     read_xml("http://www.spartanburgsheriff.org/bookings/jailrostera.xml")
 
@@ -45,7 +52,11 @@ inmates_df1 <-
         function(t) null_weeder(race[[t]])
     )) %>%
     mutate(race = unlist(race)) %>%
-    mutate(across(c(age, wt), as.numeric))
+    mutate(across(c(age, wt), as.numeric)) %>%
+    mutate(ht = return_height(ht), 
+           wt = wt / 2.2,
+           bmi = wt / ht^2) %>%
+    mutate(across(race:sex, factor))
 
 offenses <-
     inmates_df1 %>%
@@ -61,5 +72,61 @@ offenses <-
     mutate(offense = str_to_title(ol)) %>%
     select(rowid, offense)
 
+
+
 inmate_offense <-
     right_join(inmates_df1, offenses, by = "rowid", multiple = "all")
+
+
+inmate_offense %>%
+    filter(str_detect(csz, "Boiling Springs")) %>%
+    select(nl, nf, street, age, offense) %>% 
+    arrange(nl) %>% View()
+
+
+
+
+
+
+
+
+
+
+
+bmi_lines <-
+    crossing(
+        ht = seq(1.4, 2.1, .1),
+        bmi = seq(15, 40, 5)
+    ) %>%
+    mutate(wt = bmi * ht^2)
+
+bmi_labels <-
+    bmi_lines %>%
+    filter(ht == max(ht))
+
+
+inmates_df1 %>%
+    ggplot() +
+    aes(x = ht, y = wt, color = sex) +
+    geom_point() +
+    stat_ellipse() +
+    geom_line(
+        data = bmi_lines,
+        lty = 3,
+        color = "gray50",
+        aes(
+            group = factor(bmi),
+            color = NULL
+        )
+    ) +
+    geom_label(
+        data = bmi_labels,
+        aes(
+            x = ht, y = wt,
+            label = bmi,
+            color = NULL
+        )
+    ) +
+    scale_color_manual(values = c("F" = "pink", "M" = "dodgerblue")) +
+    labs(x = "Height (in m)", y = "Weight (in kg)") +
+    theme(legend.position = "none")
